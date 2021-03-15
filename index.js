cbfb3f146b59077c6b79421dbbdc134b1522b729
martin lowe <martinlowe673@gmail.com>
2021-03-15 08:25:15 +0000

    array checklist

---
+++ index.js
@@ -0,0 +1,18 @@
+const priceList = [2, 5, 7, 20, 30, 40, 55];
+
+// 1 For Loop
+for (let i = 0; i < priceList.length; i++) {
+  //console.log(priceList[i])
+}
+
+// 2. for Each
+priceList.forEach(function (element, index) {
+  //console.log(element, index)
+});
+
+//3. map function
+const result = priceList.map(function (element) {
+  return element * 2;
+});
+console.log(result);
+// doubles the result
